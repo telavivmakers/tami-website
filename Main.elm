3036398cb6d236a915bd3c6a3e9bc4feac991b30
Alon Levy <alon@pobox.com>
2015-10-11 16:04:19 +0300

    initial

---
+++ Main.elm
@@ -0,0 +1,23 @@
+module Main where
+
+
+import Html exposing (Html, text)
+
+
+main : Html
+main =
+        text """This is TAMI website. It will be a main page with links to individual
+pages from it, the parts will include:
+Events - schedule of merged google calendar plus meetup plus anything else,
+         and a way to propose events threw the website. Events include weekly, monthly,
+         and in betweens like bi-weekly (fortnight, forteen nights)
+How to get - link to wiki? or a nicer page
+Projects - link to wiki? people working on goal oriented projects.
+Birds of Feather - a collection of groups that have events as well.
+Images - latest images from facebook and the website
+News - collected latest
+Inventory - equipment with images and status, and maintainers.
+
+It might include a wiki passing stage that will give statistics on the wiki and
+activity on it
+        """
